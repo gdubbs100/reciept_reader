7baa85f5168a0875b0ff28e8e69d27ee905f45ed
unknown <grantwn1988@gmail.com>
2024-10-19 14:02:18 +1100

    created gui that shows items in editable fields

--- uploader.py
+++ uploader.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 import cv2
 import easyocr
+import ast
+import json
+
 from tkinter import filedialog
 from PIL import Image, ImageTk
 from ocr_utils import extract_items
@@ -26,17 +29,35 @@ def upload_image():
         # Print the file path below the image
         output_label.config(text=f"Uploaded Image Path: {file_path}")
 
-        # ## run the ocr  
-        # reader = easyocr.Reader(['en'])
-        # result = reader.readtext(
-        #         img,
-        #     )
-
-        # # LLM parsing
-
         items = extract_items(img)
-        # print as output
-        ocr_result.config(text=items)
+        items = items.replace('null', 'None')
+        items = ast.literal_eval(items)
+
+        frame = tk.Frame(root)
+        frame.pack(side=tk.RIGHT)
+        create_editable_fields(frame, items)
+        submit_btn = tk.Button(root, text="log data", command=lambda: write_json(items))
+        submit_btn.pack(pady=20)
+
+## I actually want to get the data from the entry fields
+def write_json(data):
+    with open('./test.json', 'w') as f:
+        json.dump(data, f)
+
+def create_editable_fields(root, data):
+
+    for idx, (key, value) in enumerate(data.items()):
+        row_label = tk.Label(root, text=key)
+        row_label.grid(row=idx * (len(value) + 1), column=0, pady=10)
+
+        for jdx, (jkey, jvalue) in enumerate(value.items()):
+            label = tk.Label(root, text=f"{jkey}:")
+            label.grid(row=idx * (len(value) + 1) + jdx + 1, column=1, padx=5)
+
+            entry= tk.Entry(root)
+            entry.grid(row=idx * (len(value)+1) + jdx + 1, column =1, padx=5)
+            entry.insert(0, str(jvalue))
+
 
 if __name__=="__main__":
 
@@ -44,21 +65,20 @@ if __name__=="__main__":
     root = tk.Tk()
     root.title("Photo Uploader")
 
+    frame = tk.Frame()
+    frame.pack(pady=20, side=tk.LEFT)
+
     # Create a button to upload image
-    upload_btn = tk.Button(root, text="Upload Photo", command=upload_image)
+    upload_btn = tk.Button(frame, text="Upload Photo", command=upload_image)
     upload_btn.pack(pady=20)
 
     # Label to display uploaded image
-    panel = tk.Label(root)
+    panel = tk.Label(frame)
     panel.pack(pady=20)
 
     # output label
-    output_label = tk.Label(root, text="",fg="blue")
+    output_label = tk.Label(frame, text="",fg="blue")
     output_label.pack(pady=20)
-
-    # ocr result
-    ocr_result = tk.Label(root, text="")
-    ocr_result.pack(pady=20)
 
     # Start the GUI event loop
     root.mainloop()
